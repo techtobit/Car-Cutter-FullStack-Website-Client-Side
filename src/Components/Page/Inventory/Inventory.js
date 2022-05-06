3b5f05ed2f1b98864e2ebd0ebd1058db49ac6669
unknown <43129513+techtobit@users.noreply.github.com>
2022-05-07 05:58:14 +0600

    Inventroy Id Ui compilted

--- src/Components/Page/Inventory/Inventory.js
+++ src/Components/Page/Inventory/Inventory.js
@@ -1,6 +1,10 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import axios from 'axios';
 import React, { useState, useEffect } from 'react';
-import { useNavigate, useParams } from 'react-router-dom';
+import { Link, useNavigate, useParams } from 'react-router-dom';
+import InventoryHeroBanner from '../../Assets/HeroBanner/hero-banner.jpg'
+import './Inventory.css'
+import { faClock, faLocationDot, faTruck, faPlus, faListCheck } from '@fortawesome/free-solid-svg-icons';
 
 
 const Inventory = () => {
@@ -36,17 +40,75 @@ const Inventory = () => {
 
   }
 
-  console.log(item.length + 1);
   return (
-    <div className='text-center grid justify-center'>
-      <h1 className='text-5xl'>This is inventory {item.length}</h1>
-      <h1 className='text-5xl'>{item.courseName}</h1>
-      <img src={item.img} alt="" />
-      <div className="btns">
-        <button onClick={handelUpdateItem} className="p-2" >Update</button>
-        <button onClick={handelAddNewItem} className="p-2">Add New</button>
-        <button onClick={() => handelDeleteItem(item._id)} className="p-2">Delete</button>
+    <div className='text-center grid '>
+      <div style={{
+        backgroundImage:
+          `url(${InventoryHeroBanner})`,
+        backgroundRepeat: 'no-repeat', backgroundPosition: 'center',
+        backgroundSize: 'cover', width: '100%',
+        height: '250px', opacity: '0.9'
+      }}>
+        <h2 className='service-title text-center text-5xl font-bold md:py-5 text-white'>
+          <span className='text-2xl'>Car</span>
+          <br />
+          <span className='service-span'>Futures & Information</span></h2>
       </div>
+      <div className='car-section grid    p-10'>
+        <div className="carData grid grid-cols-2  items-center justify-items-center border-l-4  rounded-lg border-blue-400 bg-white shadow-blue-500/50 shadow-sm shadow-cyan-500/50">
+          <div className="carImg ">
+            <img src={item.img} alt="" />
+          </div>
+          <div className="carInfo grid ">
+            <div className="carDetails grid justify-items-start pt-5">
+              <h3 className='text-2xl font-semibold'> <span className='text-black'>Product : </span> {item.carName}</h3>
+              <h3 className='text-xl font-medium'><span className='text-black'>Price : </span>$ {item.price}</h3>
+              <h3 className='text-xl font-medium text-green-600'><span className='text-black'>In Stock : </span> {item.quantity}</h3>
+              <table>
+                <tr className='flex '>
+                  <td className='font-medium'>Brand : {item.brand}</td>
+                  <td className='pl-5 font-medium'>Category : {item.category}</td>
+                </tr>
+                <tr className='flex '>
+                  <td className='font-medium'>Color : {item.color}</td>
+                  <td className='pl-5 font-medium'>System : {item.type}</td>
+                </tr>
+                <tr className='flex '>
+                  <td className='font-medium'>Engin : {item.engin}</td>
+                  <td className='pl-5 font-medium'>Supplier : {item.supplier}</td>
+                </tr>
+                <tr className='flex '>
+                  <td className='font-medium'>
+                    <FontAwesomeIcon className='text-blue-500 pr-2' icon={faClock}></FontAwesomeIcon>
+                    Release : {item.releaseDate}</td>
+                  <td className='pl-5 font-medium'>
+                    <FontAwesomeIcon className='text-blue-500 pr-2' icon={faLocationDot} />Warehouse :
+                    {item.warehouse}</td>
+                </tr>
+              </table>
+            </div>
+
+            <div className="btns  pb-5">
+              <div className="reStock-btn">
+                <input type="number" name='quantity' />
+                <input type="button" value='Increase Stock' />
+              </div>
+              <div className="manage-bnt">
+                <button className="p-2 bg-yellow-400 rounded-md text-black font-semibold">
+                  <FontAwesomeIcon className='pr-2' icon={faTruck}></FontAwesomeIcon>
+                  Delivered</button>
+                <button onClick={handelAddNewItem} className="p-2 bg-green-600 rounded-md text-white font-semibold">
+                  <FontAwesomeIcon className='pr-2' icon={faPlus}></FontAwesomeIcon>
+                  Add New</button>
+                <Link to='/manage' className="p-2 bg-orange-600 rounded-md text-white font-semibold">
+                  <FontAwesomeIcon className='pr-2' icon={faListCheck}></FontAwesomeIcon>
+                  Manage</Link>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
     </div>
   );
 };
